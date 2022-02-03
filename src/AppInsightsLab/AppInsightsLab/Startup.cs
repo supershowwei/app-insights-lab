0d9b3a5b056a7b9f7cda14e41c3724fa3aab9d99
supershowwei <supershowwei@gmail.com>
2022-02-03 17:44:35 +0800

    connect to Application Insights

--- src/AppInsightsLab/AppInsightsLab/Startup.cs
+++ src/AppInsightsLab/AppInsightsLab/Startup.cs
@@ -23,6 +23,7 @@ namespace AppInsightsLab
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddApplicationInsightsTelemetry(this.Configuration["ApplicationInsights:InstrumentationKey"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
